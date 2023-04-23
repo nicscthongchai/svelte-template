35df593d7ed0980bc9041a30b48703d69ef6d2a9
nicscthongchai <nicscthongchai@gmail.com>
2023-04-23 15:45:34 +0700

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import '../styles/app.css';
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		if (
+			localStorage.theme === 'dark' ||
+			(!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+		) {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
+	});
+</script>
+
+<main class="flex min-h-full flex-col dark:bg-gray-900 dark:text-white">
+	<slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<section class="text-center">
+	<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
+	<p class="mt-5">
+		Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+	</p>
+</section>
