dbb0ea0996b51f4e1e0cf9d5ebba54aaf30927a8
nicscthongchai <nicscthongchai@gmail.com>
2023-07-11 18:17:53 +0700

    Initialized template

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import 'app.css';
+
+	onMount(async () => {
+		if (
+			localStorage.theme === 'dark' ||
+			(!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+		) {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
+	});
+</script>
+
+<main class="flex min-h-full flex-col dark:bg-gray-900 dark:text-white">
+	<slot />
+</main>
